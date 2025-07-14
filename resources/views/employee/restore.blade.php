af37099a92b304da9b76e2f61f49608d061417d4
Kavya Timalsina <kavya.timilsina1230@gmail.com>
2025-07-14 12:45:38 +0545

    changed placeholder name

--- resources/views/employee/restore.blade.php
+++ resources/views/employee/restore.blade.php
@@ -24,7 +24,7 @@
                     <label for="search" class="block text-sm font-medium text-gray-700 mb-1">Search</label>
                     <input id="search" name="search" value="{{ request()->search }}"
                         class="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500"
-                        placeholder="Search by name or email" />
+                        placeholder="Search by First Name, Last Name" />
                 </div>
 
                 <div>
--- resources/views/employee/view.blade.php
+++ resources/views/employee/view.blade.php
@@ -32,7 +32,7 @@
                 <div>
                     <label for="search" class="block text-sm font-medium text-gray-700 mb-1">Search</label>
                     <input id="search" name="search" value="{{ request()->search }}"
-                        placeholder="Search by name, email..."
+                        placeholder="Search by First Name, Last Name"
                         class="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500" />
                 </div>
 
